8d0cfbd9bf568a6b6fd6ea6a911e691c815dc757
jolorambongga <151989553+jolorambongga@users.noreply.github.com>
2024-06-23 23:20:31 +0800

    Add files via upload

--- client/index.php
+++ client/index.php
@@ -9,14 +9,14 @@ include_once('header.php');
 
 <body>
     <!-- nav bar -->
-  <div style="background-color: #FFC000; width: 100%; height: 700px;">
+  <div style="background-image: url(https://wallpaperaccess.com/full/1282798.jpg); width: 100%; height: 700px;">
     <div class="my-wrapper">
         <div class="navbar navbar-expand-lg" style="margin-bottom: 45px; margin-top: 50px;">
             <div class="container-fluid justify-content-center">
                 <a class="navbar-brand d-flex flex-column align-items-center" href="index.php">
                     <img src="https://www.logolynx.com/images/logolynx/2a/2ad00c896e94f1f42c33c5a71090ad5e.png" alt="Logo" width="150" height="auto" class="d-inline-block align-text-top mr-2" style="margin-bottom: 30px;">
-                    <div class="brand-text" style="font-size: 45px; font-weight: bold; line-height: 1;">STA. MARIA DIAGNOSTIC</div>
-                    <div class="brand-text" style="font-size: 45px; font-weight: bold; line-height: 1;">AND IMAGING CENTER</div>
+                    <div class="brand-text" style="font-size: 45px; color: white; font-weight: bold; line-height: 1;">STA. MARIA DIAGNOSTIC</div>
+                    <div class="brand-text" style="font-size: 45px; color: white; font-weight: bold; line-height: 1;">AND IMAGING CENTER</div>
                 </a>
             </div>
         </div>
@@ -38,7 +38,7 @@ include_once('header.php');
     </div>
 </div>
 <div style="text-align: center; margin-bottom: 60px; background-color: white">
-  <img src="../step.png" alt="steps" style="width: 800px; height: auto; display: block; margin-left: auto; margin-right: auto;">
+  <img src="https://cdn.discordapp.com/attachments/489358237343416320/1254446687850729512/step.png?ex=66798604&is=66783484&hm=9a73d53787e6a67ea08b03b820667b1c901ebc14c8456c9eb9eabc24e9db31e1&" alt="steps" style="width: 800px; height: auto; display: block; margin-left: auto; margin-right: auto;">
 </div>
     <!-- end nav bar -->
     <div class="my-wrapper" style="margin-bottom: 50px;">
