76a1facca784b60f478f2963915ce811648f864b
jolorambongga <151989553+jolorambongga@users.noreply.github.com>
2024-06-24 06:33:33 +0800

    Add files via upload

--- admin/landing.php
+++ admin/landing.php
@@ -57,51 +57,51 @@ include_once('header.php');
                 <!-- about us image -->
                 <label for="about_us_image" class="form-label">About Us Image</label>
                 <div class="form-check mb-3">
-                  <input class="form-check-input" type="radio" name="about_us_image_option" id="about_us_image_url" value="url">
+                  <input class="form-check-input" type="radio" name="about_us_image_option" id="about_us_image_url" value="url" required>
                   <label class="form-check-label" for="about_us_image_url">
                     URL
                   </label>
                 </div>
                 <div class="form-check mb-3">
-                  <input class="form-check-input" type="radio" name="about_us_image_option" id="about_us_image_upload" value="upload">
+                  <input class="form-check-input" type="radio" name="about_us_image_option" id="about_us_image_upload" value="upload" required>
                   <label class="form-check-label" for="about_us_image_upload">
                     Upload Image
                   </label>
                 </div>
                 <div class="input-group mb-3 about-us-image-url" style="display: none;">
-                  <input type="url" class="form-control" id="about_us_image_url_input" placeholder="Enter URL">
+                  <input type="url" class="form-control" id="about_us_image_url_input" placeholder="Enter URL" required>
                 </div>
                 <div class="input-group mb-3 about-us-image-upload" style="display: none;">
-                  <input type="file" class="form-control" id="about_us_image_upload_input">
+                  <input type="file" class="form-control" id="about_us_image_upload_input" required>
                 </div>
                 <pre></pre>
 
                 <!-- main image -->
                 <label for="main_image" class="form-label">Main Image</label>
                 <div class="form-check mb-3">
-                  <input class="form-check-input" type="radio" name="main_image_option" id="main_image_url" value="url">
+                  <input class="form-check-input" type="radio" name="main_image_option" id="main_image_url" value="url" required>
                   <label class="form-check-label" for="main_image_url">
                     URL
                   </label>
                 </div>
                 <div class="form-check mb-3">
-                  <input class="form-check-input" type="radio" name="main_image_option" id="main_image_upload" value="upload">
+                  <input class="form-check-input" type="radio" name="main_image_option" id="main_image_upload" value="upload" required>
                   <label class="form-check-label" for="main_image_upload">
                     Upload Image
                   </label>
                 </div>
                 <div class="input-group mb-3 main-image-url" style="display: none;">
-                  <input type="url" class="form-control" id="main_image_url_input" placeholder="Enter URL">
+                  <input type="url" class="form-control" id="main_image_url_input" placeholder="Enter URL" required>
                 </div>
                 <div class="input-group mb-3 main-image-upload" style="display: none;">
-                  <input type="file" class="form-control" id="main_image_upload_input">
+                  <input type="file" class="form-control" id="main_image_upload_input" required>
                 </div>
                 <pre></pre>
 
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-                <button type="submit" class="btn btn-success">Save Changes</button>
+                <button id="btnSave" type="button" class="btn btn-success">Save Changes</button>
               </div>
             </div>
           </div>
@@ -119,9 +119,13 @@ include_once('header.php');
         if (this.value === 'url') {
           $('.about-us-image-url').show();
           $('.about-us-image-upload').hide();
+          $('#about_us_image_upload_input').removeAttr('required');
+          $('#about_us_image_url_input').prop('required', true);
         } else if (this.value === 'upload') {
           $('.about-us-image-url').hide();
           $('.about-us-image-upload').show();
+          $('#about_us_image_url_input').removeAttr('required');
+          $('#about_us_image_upload_input').prop('required', true);
         }
       });
 
@@ -129,10 +133,28 @@ include_once('header.php');
         if (this.value === 'url') {
           $('.main-image-url').show();
           $('.main-image-upload').hide();
+          $('#main_image_upload_input').removeAttr('required');
+          $('#main_image_url_input').prop('required', true);
         } else if (this.value === 'upload') {
           $('.main-image-url').hide();
           $('.main-image-upload').show();
+          $('#main_image_url_input').removeAttr('required');
+          $('#main_image_upload_input').prop('required', true);
         }
+      });
+
+      $(document).on('click', '#btnSave', function() {
+        $.ajax({
+          type: 'GET',
+          dataType: 'JSON',
+          url: 'handles/landing/create_landing',
+          success: function(response) {
+            console.log("SAVE LANDING RESPONSE", response);
+          },
+          error: function(error) {
+            console.log("ERROR LANDING RESPONSE", error);
+          }
+        })
       });
     });
   </script>
